61c2ae955ff427d9378ee2b5ed3830d0b80e0daf
CarlosJulioRuizTovar <73555964+CarlosJulioRuizTovar@users.noreply.github.com>
2022-12-09 02:01:09 -0500

    Add files via upload

---
+++ 2022-11-28-OOP-Particles-Visualization/main_md_damp.cpp
@@ -0,0 +1,45 @@
+#include "Particle.h"
+#include "Integrator.h"
+#include "Collider.h"
+#include <vector>
+
+int main(int argc, char **argv) {
+  std::vector<Particle> bodies;
+  bodies.resize(1); // only one particle for now
+
+  // parameters
+  std::map<std::string, double> p;
+  p["T0"] = 0.0;
+  p["TF"] = 10.8767;
+  p["DT"] = 0.01;
+  p["K"] = 207.76;
+  p["G"] = 9.81;
+  p["B"] = 0.671;
+  p["L"] = 4.1;
+
+  // Force collider
+  Collider collider(p);
+
+  // Time initialization
+  TimeIntegrator integrator(p["DT"]);
+
+  // initial conditions and properties
+  bodies[0].R[2] = 0.987;
+  bodies[0].rad  = 0.103;
+  bodies[0].mass = 0.337;
+  bodies[0].V[0] = - 1.127;
+  collider.computeForces(bodies); // force at t = 0
+  integrator.startIntegration(bodies); // start integration algorithm
+  std::cout << p["T0"] << "\t" << bodies[0] << "\n";
+
+  // Time iteration
+  const int niter = int((p["TF"] - p["T0"])/p["DT"]);
+  for(int ii = 1; ii < niter; ++ ii) {
+    collider.computeForces(bodies);
+    integrator.timeStep(bodies);
+    double time = p["T0"] + ii * p["DT"];
+    std::cout << time << "\t" << bodies[0] << "\n";
+  }
+
+  return 0;
+}
